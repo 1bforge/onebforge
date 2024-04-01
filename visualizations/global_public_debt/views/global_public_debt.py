7a72dc0b9d751396aebf7cebfc9207f163a1f4fa
1bforge <onebforge@gmail.com>
2024-04-01 21:50:21 +0200

    plotly bar animation added

--- visualizations/global_public_debt/views/global_public_debt.py
+++ visualizations/global_public_debt/views/global_public_debt.py
@@ -15,6 +15,48 @@ class DebtView():
                 years.append(col)
             else:
                 data = data.rename(columns={col: 'Country'})
+        
+        data = data.dropna()
+
+        # Visualization
+        bar_data = pd.melt(data, id_vars=['Country'], var_name='Year', value_name='Value')
+        bar_data.loc[bar_data['Value']=='no data', 'Value'] = 0
+
+        years_to_show = st.slider(
+            "Select the years to be visualized", 
+            bar_data['Year'].min(), 
+            bar_data['Year'].max(), 
+            value=(
+                bar_data['Year'].min(), 
+                bar_data['Year'].max()
+            ), 
+            step=1, 
+        )
+        first_year = years_to_show[0]
+        last_year = years_to_show[1]
+
+        bar_data = bar_data[(bar_data['Year'] >= first_year) & (bar_data['Year'] <= last_year)].reset_index(drop=True)
+
+        fig_scatter = px.bar(
+            data_frame=bar_data,
+            x='Value',
+            y='Country',
+            animation_frame='Year',
+            animation_group='Country',
+            color='Country',
+            orientation='h',
+            range_x=[0, bar_data['Value'].max()],
+            labels={'value':'Value'}
+        )
+        fig_scatter.update_traces(
+            texttemplate='%{value:.2f}',
+            textposition='auto', 
+        )
+        fig_scatter.update_yaxes(categoryorder="total ascending")
+        fig_scatter.layout.updatemenus[0].buttons[0].args[1]["frame"]["duration"] = 750
+        fig_scatter.layout.update(showlegend=False)
+        st.plotly_chart(fig_scatter, use_container_width=True)
+
         selected_year = st.selectbox("Select the year to visualize:", years)
 
         plot_data = data.copy()
@@ -43,12 +85,12 @@ class DebtView():
                 coastlinecolor='#99ddcc'
             ),
             title_font_color="#99ddcc",
-            height=800,  # Adjust the height as needed
-            width=2000,   # Adjust the width as needed
+            # height=800,  # Adjust the height as needed
+            # width=2000,   # Adjust the width as needed
         )
 
         # Show the map using Streamlit
-        st.plotly_chart(fig1)
+        st.plotly_chart(fig1, use_container_width=True)
 
         trend_values = []
         for year in years:
@@ -65,10 +107,10 @@ class DebtView():
 
         fig2.update_layout(
             title_font_color="#99ddcc",
-            width=2000,  # Adjust the width as needed
-            height=800,  # Adjust the height as needed
+            # width=2000,  # Adjust the width as needed
+            # height=800,  # Adjust the height as needed
         )
         # Show the chart using Streamlit
-        st.plotly_chart(fig2)
+        st.plotly_chart(fig2, use_container_width=True)
 
 
